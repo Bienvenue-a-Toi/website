91d7db1994336380fc9dcc2dbe37e6c8af8d8819
Adrien Allard <adrien.allard.pro@gmail.com>
2026-03-02 20:05:37 +0100

    feat: menu responsiveness

--- README.md
+++ README.md
@@ -1,3 +1,18 @@
 # Bienvenue à Toi
 
 Dépôt du site de l'association Bienvenue à Toi.
+
+## Dev env
+
+Install the following dependencies to be able to test the website locally:
+
+```sh
+npm install -g browser-sync
+```
+
+Then it's possible to start a local server using the command:
+
+```sh
+browser-sync start --server --watch
+```
+
---
+++ index.js
@@ -0,0 +1,25 @@
+class ToggleNavBarButton extends HTMLElement {
+  connectedCallback() {
+    this.addEventListener('click', this.onClick.bind(this))
+    const windowsChangeEvent = ('onorientationchange' in window) ? 'orientationchange' : 'resize';
+    window.addEventListener(windowsChangeEvent, this.close.bind(this))
+  }
+
+  get navbar() {
+    const navbar = this.closest('.primary-nav')
+    console.assert(navbar !== undefined)
+    return navbar
+  }
+
+  onClick(evt) {
+    this.navbar.classList.toggle('primary-nav__open')
+    evt.preventDefault()
+  }
+
+  close() {
+    this.navbar.classList.remove('primary-nav__open')
+  }
+}
+
+window.customElements.define('toggle-nav-bar-button', ToggleNavBarButton)
+
