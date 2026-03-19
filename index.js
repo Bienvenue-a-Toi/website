5ff39d0e731d26240107a3812077de8db7f7551e
Adrien Allard <adrien.allard.pro@gmail.com>
2026-03-19 17:11:22 +0100

    feat: add programmation

--- index.js
+++ index.js
@@ -31,3 +31,51 @@ class ToggleNavBarButton extends HTMLElement {
 }
 
 window.customElements.define('toggle-nav-bar-button', ToggleNavBarButton)
+
+var currentOppenedCard = null
+class ArtistCard extends HTMLElement {
+  connectedCallback() {
+    this.addEventListener('click', this.onClick.bind(this))
+    // const windowsChangeEvent = ('onorientationchange' in window) ? 'orientationchange' : 'resize';
+    // window.addEventListener(windowsChangeEvent, this.close.bind(this))
+  }
+
+  get details() {
+    const details = this.querySelector('.programme-card__artist__details')
+    return details
+  }
+
+  onClick(evt) {
+    if (!this.details) {
+      evt.preventDefault()
+      return
+    }
+    this.details.classList.toggle('programme-card__artist__details--open')
+
+    if (this.details.classList.contains('programme-card__artist__details--open')) {
+      if (currentOppenedCard != null) {
+        currentOppenedCard.close()
+      }
+      currentOppenedCard = this
+      this.scrollIntoView({ behavior: "smooth", block: "start" })
+    }
+    else {
+      if (currentOppenedCard === this) {
+        currentOppenedCard = null;
+      }
+    }
+    evt.preventDefault()
+  }
+
+  close() {
+    if (!this.details) {
+      return
+    }
+    this.details.classList.remove('programme-card__artist__details--open')
+    if (currentOppenedCard === this) {
+      currentOppenedCard = null;
+    }
+  }
+}
+
+window.customElements.define('artist-card', ArtistCard)
